66ab134ac639af24a75e82543de34802527c802d
gabisaldivar <saldivargaby18@gmail.com>
2019-10-23 23:53:05 +0300

    Revert "First"
    This reverts commit 0786c095

--- src/lesson1/JavaTasks.java
+++ src/lesson1/JavaTasks.java
@@ -1,94 +1,42 @@
 package lesson1;
 
 import kotlin.NotImplementedError;
-import kotlin.Pair;
-import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.*;
 
 @SuppressWarnings("unused")
 public class JavaTasks {
     /**
      * Сортировка времён
-     * <p>
+     *
      * Простая
      * (Модифицированная задача с сайта acmp.ru)
-     * <p>
+     *
      * Во входном файле с именем inputName содержатся моменты времени в формате ЧЧ:ММ:СС AM/PM,
      * каждый на отдельной строке. См. статью википедии "12-часовой формат времени".
-     * <p>
+     *
      * Пример:
-     * <p>
+     *
      * 01:15:19 PM
      * 07:26:57 AM
      * 10:00:03 AM
      * 07:56:14 PM
      * 01:15:19 PM
      * 12:40:31 AM
-     * <p>
+     *
      * Отсортировать моменты времени по возрастанию и вывести их в выходной файл с именем outputName,
      * сохраняя формат ЧЧ:ММ:СС AM/PM. Одинаковые моменты времени выводить друг за другом. Пример:
-     * <p>
+     *
      * 12:40:31 AM
      * 07:26:57 AM
      * 10:00:03 AM
      * 01:15:19 PM
      * 01:15:19 PM
      * 07:56:14 PM
-     * <p>
+     *
      * В случае обнаружения неверного формата файла бросить любое исключение.
      */
-    static public void sortTimes(String inputName, String outputName) throws Exception {
-       List<Pair<Integer, String>> timeDate = new ArrayList<>();
-        List<Integer> minute = new ArrayList<>();
-        BufferedReader br = new BufferedReader(new FileReader(inputName));
-        String line;
-        while ((line = br.readLine()) != null) {
-            if (line.contains("AM")) {
-                String stringI = line.replaceAll(" AM", "");
-                String[] split = stringI.split(":");
-                int hour = Integer.parseInt(split[0]);
-                int minutes = Integer.parseInt(split[1]);
-                int seconds = Integer.parseInt(split[2]);
-                if (hour == 12) hour = 0;
-                int secondsTime = (hour * 60 + minutes) * 60 + seconds;
-                timeDate.add(new Pair<>(secondsTime, line));
-            } else if (line.contains("PM")) {
-                String stringI = line.replaceAll(" PM", "");
-                String[] split = stringI.split(":");
-                int hourI = Integer.parseInt(split[0]);
-                int minutes = Integer.parseInt(split[1]);
-                int seconds = Integer.parseInt(split[2]);
-                if (hourI == 12) {
-                    int secondsTime = ((hourI) * 60 + minutes) * 60 + seconds;
-                    timeDate.add(new Pair<>(secondsTime, line));
-                } else {
-                    int secondsTime = ((hourI + 12) * 60 + minutes) * 60 + seconds;
-                    timeDate.add(new Pair<>(secondsTime, line));
-                }
-            }
-        }
-        for (Pair<Integer, String> pair : timeDate) {
-            Integer key = pair.getFirst();
-            String value = pair.getSecond();
-            minute.add(key);
-        }
-        minute.sort(Comparator.naturalOrder());
-
-        FileOutputStream writer = new FileOutputStream(outputName);
-        for (Integer integer : minute) {
-            for (Pair<Integer, String> integerStringPair : timeDate) {
-                if (integer == integerStringPair.getFirst()) {
-                    writer.write(integerStringPair.getSecond().getBytes());
-                    writer.write("\n".getBytes());
-                }
-            }
-        }
-
-        writer.close();
-}
+    static public void sortTimes(String inputName, String outputName) {
+        throw new NotImplementedError();
+    }
 
     /**
      * Сортировка адресов
@@ -116,59 +64,8 @@ public class JavaTasks {
      *
      * В случае обнаружения неверного формата файла бросить любое исключение.
      */
-    static public void sortAddresses(String inputName, String outputName) throws Exception {
-        ArrayList<Pair<String, Integer>> streetAndNumberList = new ArrayList<>();
-        //List<String> personList;
-        List<List> personList = new ArrayList<>();
-        Map<String, String> map = new HashMap<>();
-
-        Path pathFile = Paths.get(inputName);
-        long numberLines = Files.lines(pathFile).count();
-
-        //try (BufferedReader br = new BufferedReader(new FileReader(new File(inputName)))) {
-       // try (BufferedReader br = new BufferedReader(new InputStreamReader(inputName), "UTF8")) {
-        BufferedReader br = new BufferedReader(new InputStreamReader(
-                new FileInputStream(inputName), "UTF-8"));
-            String line;
-            while ((line = br.readLine()) != null) {
-                //String [] streetAndNumber = line.replaceAll("\\s-\\s[а-яА-Я]+\\s[1-9]+", "").split(" ");
-                    //String person[] = new String[3];
-                    List<String> person = new ArrayList<String>();
-                    List<Integer> personi = new ArrayList<Integer>();
-                    List<List> personl = new ArrayList<List>();
-                    String dirAndNumber = line.replaceAll("[а-яА-Я]+\\s[а-яА-Я]+\\s+-\\s", "").trim();
-                    String direction = dirAndNumber.split(" ")[0];
-                    Integer numberHouse = Integer.parseInt(dirAndNumber.split(" ")[1]);
-                    String namesPerson = line.replaceAll("\\s-\\s[а-яА-Я]+\\s[1-9]+", "");
-                    List <String> namePersonList = new ArrayList<String>();
-                    namePersonList.add(namesPerson);
-                    person.add(direction);
-                    personi.add(numberHouse);
-                    personl.add(namePersonList);
-                    List<List> listfinal = new ArrayList<List>();
-                    listfinal.add(person);
-                listfinal.add(personi);
-                listfinal.add(personl);
-                personList.add(listfinal);
-
-
-
-                    //personList = Arrays.asList(person));
-                    //personList.forEach(System.out::println);
-                    //streetAndNumberList.add(new Pair(streetAndNumber[0], streetAndNumber[1]));
-                    //personList.add(new Pair(person[0], person[1]));
-                    //map.put(streetAndNumber[1], streetAndNumber[0]);
-                    FileOutputStream writer = new FileOutputStream(outputName);
-                    writer.write(line.getBytes());
-                    writer.flush();
-                    writer.close();
-
-       /* Comparator<Pair<String, String>> comparatorA = comparing(Pair<String, String>::getFirst);
-        Comparator <Pair<String, String>> comparatorB = comparatorA.thenComparing((Pair::getSecond));
-        personList.forEach(System.out::println);*/
-
-            }
-        //}
+    static public void sortAddresses(String inputName, String outputName) {
+        throw new NotImplementedError();
     }
 
     /**
@@ -201,15 +98,8 @@ public class JavaTasks {
      * 99.5
      * 121.3
      */
-    static public void sortTemperatures(String inputName, String outputName) throws Exception {
-        BufferedReader r = new BufferedReader(new FileReader(inputName));
-        String line;
-        while ((line = r.readLine()) != null) {
-            //String streetAndNumber = line.replaceAll(".", "");
-            System.out.println(line);
-
-        }
-        FileOutputStream writer = new FileOutputStream(outputName);
+    static public void sortTemperatures(String inputName, String outputName) {
+        throw new NotImplementedError();
     }
 
     /**
@@ -263,4 +153,3 @@ public class JavaTasks {
         throw new NotImplementedError();
     }
 }
-
